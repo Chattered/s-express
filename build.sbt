daa7f8febb571e7adc99dd3fa00dc290621ccf23
Phil Scott <me@philscotted.com>
2016-09-15 19:23:09 +0100

    Initial commit.

---
+++ build.sbt
@@ -0,0 +1,21 @@
+name := "s-express"
+lazy val root = project.in(file(".")).
+  aggregate(sexpressJS, sexpressJVM).
+  settings(
+    publish := {},
+    publishLocal := {}
+  )
+lazy val sexpress = crossProject.in(file(".")).
+  settings(
+    name := "s-express",
+    organization := "org.ensime",
+    version := "2.0.0-SNAPSHOT",
+    scalaVersion in ThisBuild := "2.11.8",
+    scalacOptions += "-feature",
+    libraryDependencies ++= Seq(
+      "org.parboiled" %% "parboiled" % "2.1.2",
+      "com.typesafe.akka" %% "akka-slf4j" % "2.3.15"
+    )
+  )
+lazy val sexpressJVM = sexpress.jvm
+lazy val sexpressJS  = sexpress.js
---
+++ shared/src/test/scala/org/ensime/sexp/SexpSpec.scala
@@ -0,0 +1,24 @@
+// Copyright: 2010 - 2016 https://github.com/ensime/ensime-server/graphs
+// License: http://www.gnu.org/licenses/lgpl-3.0.en.html
+package org.ensime.sexp
+
+import org.scalatest._
+import org.scalactic.TypeCheckedTripleEquals
+import org.slf4j.LoggerFactory
+import org.slf4j.bridge.SLF4JBridgeHandler
+
+/**
+ * Boilerplate remover and preferred testing style in S-Express.
+ */
+abstract class SexpSpec extends FlatSpec
+    with Matchers
+    with Inside
+    with TryValues
+    with Inspectors
+    with TypeCheckedTripleEquals {
+
+  SLF4JBridgeHandler.removeHandlersForRootLogger()
+  SLF4JBridgeHandler.install()
+  val log = LoggerFactory.getLogger(this.getClass)
+
+}
